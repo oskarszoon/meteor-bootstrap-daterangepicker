1aca580cb126e7a3177a00cadb1fe10d41838af7
Oli Oskarsson <oli@oskarsson.nl>
2014-03-09 14:49:50 +0100

    Upped version

--- package.js
+++ package.js
@@ -1,11 +1,10 @@
 Package.describe({
-	summary: "This date range picker component for Twitter Bootstrap creates a dropdown menu from which a user can select a range of dates."
+    summary: "This date range picker component for Twitter Bootstrap creates a dropdown menu from which a user can select a range of dates."
 });
 
-Package.on_use(function (api) {
-	api.add_files([
-		'lib/bootstrap-daterangepicker/daterangepicker.css',
-		'lib/bootstrap-daterangepicker/date.js',
-		'lib/bootstrap-daterangepicker/daterangepicker.js'
-		], 'client');
+Package.on_use(function(api) {
+    api.add_files([
+        'lib/bootstrap-daterangepicker/daterangepicker-bs3.css',
+        'lib/bootstrap-daterangepicker/daterangepicker.js'
+    ], 'client');
 });
