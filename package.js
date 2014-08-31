b7602f74acfd9f54d31ed9e9d6dcad0673bebb63
Oli Oskarsson <oli@oskarsson.nl>
2014-08-31 21:14:41 +0200

    Added missing moment requirement

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
     summary: "Date range picker component for Twitter Bootstrap 3",
     "name": "loftsteinn:bootstrap3-daterangepicker",
-    version: "0.3.0",
+    version: "0.3.1",
     git: "https://github.com/oskarszoon/meteor-bootstrap-daterangepicker"
 });
 
 Package.on_use(function(api) {
+    api.use('mrt:moment@2.8.1', 'client');
     api.add_files([
         'lib/bootstrap-daterangepicker/daterangepicker-bs3.css',
         'lib/bootstrap-daterangepicker/daterangepicker.js'
