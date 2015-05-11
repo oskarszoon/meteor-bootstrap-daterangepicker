f0cc137cbd168731f32f3782ce93e2e3a9d85d76
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-05-11 11:27:16 +0800

    Bump version

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     summary: "Date range picker component for Twitter Bootstrap 3",
-    "name": "loftsteinn:bootstrap3-daterangepicker",
-    version: "0.3.1",
+    "name": "gilbertwat:bootstrap3-daterangepicker",
+    version: "1.3.21",
     git: "https://github.com/oskarszoon/meteor-bootstrap-daterangepicker"
 });
 
 Package.on_use(function(api) {
-    api.use('mrt:moment@2.8.1', 'client');
+    api.use('mrt:moment@2.9.0', 'client');
     api.add_files([
         'lib/bootstrap-daterangepicker/daterangepicker-bs3.css',
         'lib/bootstrap-daterangepicker/daterangepicker.js'
